a28b52075e33dc362fbd6cf63a467189a3a693e8
catiga <catiga03@gmail.com>
2022-04-19 11:17:20 +0800

    '替换cellarray'

--- src/com/smartgrid/dto/basic/DataBasicBus.java
+++ src/com/smartgrid/dto/basic/DataBasicBus.java
@@ -1,10 +1,11 @@
 package com.smartgrid.dto.basic;
 
+import com.mathworks.toolbox.javabuilder.MWCellArray;
 import com.mathworks.toolbox.javabuilder.MWStructArray;
 
 public class DataBasicBus {
 
-    String[] busname;
+    MWCellArray busname;
 
     double[] busbase;
 
@@ -15,16 +16,23 @@ public class DataBasicBus {
     }
 
     public DataBasicBus() {
-        busname = new String[] {"鄂云梦220","鄂红石坡220","鄂孝感220","鄂孝感1B220","鄂姚家冲220","鄂黄石210","鄂黄新厂220","鄂红石坡110","鄂红石坡11-1"};
+        String[] tmp_busname = new String[] {"鄂云梦220","鄂红石坡220","鄂孝感220","鄂孝感1B220","鄂姚家冲220","鄂黄石210","鄂黄新厂220","鄂红石坡110","鄂红石坡11-1"};
         busbase = new double[] {230.0};
         stationname = new String[] {"鄂云梦220","鄂红石坡220","鄂孝感220","鄂孝感1B220","鄂姚家冲220","鄂黄石210","鄂黄新厂220","鄂红石坡110","鄂红石坡11-1"};
+        
+        MWCellArray cell = new MWCellArray(new int[]{1, tmp_busname.length});
+        int i = 1;
+        for(String s : tmp_busname) {
+            int[] idx = new int[] {1, i++};
+            cell.set(idx, s);
+        }
     }
 
-    public String[] getBusname() {
+    public MWCellArray getBusname() {
         return busname;
     }
 
-    public void setBusname(String[] busname) {
+    public void setBusname(MWCellArray busname) {
         this.busname = busname;
     }
 
