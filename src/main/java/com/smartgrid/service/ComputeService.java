d644da8630cf22e2b8213fa44791ff3ff14c7297
catiga <catiga03@gmail.com>
2022-05-13 17:18:34 +0800

    2

--- src/main/java/com/smartgrid/service/ComputeService.java
+++ src/main/java/com/smartgrid/service/ComputeService.java
@@ -135,9 +135,78 @@ public class ComputeService {
         //开始构建输入参数
         MWCellArray Table_nodes_level_province_area = new MWCellArray(new int[] {tableNodeData.size(), 3});
         double[][] bus_level = new double[busLevelData.size()][13];
-        double[][] generator_level = new double[generatorLevelData.size()][13];
-        double[][] branch_level = new double[generatorLevelData.size()][13];
+        double[][] generator_level = new double[generatorLevelData.size()][21];
+        double[][] branch_level = new double[branchLevelData.size()][13];
         String[] miantance_Target = new String[] {taskData.get(0).getStationName()};
+        
+        for(int i=0; i<tableNodeData.size(); i++) {
+        	int[] idx1 = new int[] {i+1, 1};
+        	int[] idx2 = new int[] {i+1, 2};
+        	int[] idx3 = new int[] {i+1, 3};
+        	Table_nodes_level_province_area.set(idx1, tableNodeData.get(i).getV1().doubleValue());
+        	Table_nodes_level_province_area.set(idx2, tableNodeData.get(i).getV2());
+        	Table_nodes_level_province_area.set(idx3, tableNodeData.get(i).getV3().doubleValue());
+        }
+        
+        for(int i=0; i<busLevelData.size(); i++) {
+        	bus_level[i][0] = busLevelData.get(i).getBl1().doubleValue();
+        	bus_level[i][1] = busLevelData.get(i).getBl2().doubleValue();
+        	bus_level[i][2] = busLevelData.get(i).getBl3().doubleValue();
+        	bus_level[i][3] = busLevelData.get(i).getBl4().doubleValue();
+        	bus_level[i][4] = busLevelData.get(i).getBl5().doubleValue();
+        	bus_level[i][5] = busLevelData.get(i).getBl6().doubleValue();
+        	bus_level[i][6] = busLevelData.get(i).getBl7().doubleValue();
+        	bus_level[i][7] = busLevelData.get(i).getBl8().doubleValue();
+        	bus_level[i][8] = busLevelData.get(i).getBl9().doubleValue();
+        	bus_level[i][9] = busLevelData.get(i).getBl10().doubleValue();
+        	bus_level[i][10] = busLevelData.get(i).getBl11().doubleValue();
+        	bus_level[i][11] = busLevelData.get(i).getBl12().doubleValue();
+        	bus_level[i][12] = busLevelData.get(i).getBl13().doubleValue();
+        }
+        
+        for(int i=0; i<generatorLevelData.size(); i++) {
+        	generator_level[i][0] = generatorLevelData.get(i).getGl1().doubleValue();
+        	generator_level[i][1] = generatorLevelData.get(i).getGl2().doubleValue();
+        	generator_level[i][2] = generatorLevelData.get(i).getGl3().doubleValue();
+        	generator_level[i][3] = generatorLevelData.get(i).getGl4().doubleValue();
+        	generator_level[i][4] = generatorLevelData.get(i).getGl5().doubleValue();
+        	generator_level[i][5] = generatorLevelData.get(i).getGl6().doubleValue();
+        	generator_level[i][6] = generatorLevelData.get(i).getGl7().doubleValue();
+        	generator_level[i][7] = generatorLevelData.get(i).getGl8().doubleValue();
+        	generator_level[i][8] = generatorLevelData.get(i).getGl9().doubleValue();
+        	generator_level[i][9] = generatorLevelData.get(i).getGl10().doubleValue();
+        	generator_level[i][10] = generatorLevelData.get(i).getGl11().doubleValue();
+        	generator_level[i][11] = generatorLevelData.get(i).getGl12().doubleValue();
+        	generator_level[i][12] = generatorLevelData.get(i).getGl13().doubleValue();
+        	generator_level[i][13] = generatorLevelData.get(i).getGl14().doubleValue();
+        	generator_level[i][14] = generatorLevelData.get(i).getGl15().doubleValue();
+        	generator_level[i][15] = generatorLevelData.get(i).getGl16().doubleValue();
+        	generator_level[i][16] = generatorLevelData.get(i).getGl17().doubleValue();
+        	generator_level[i][17] = generatorLevelData.get(i).getGl18().doubleValue();
+        	generator_level[i][18] = generatorLevelData.get(i).getGl19().doubleValue();
+        	generator_level[i][19] = generatorLevelData.get(i).getGl20().doubleValue();
+        	generator_level[i][20] = generatorLevelData.get(i).getGl21().doubleValue();
+        }
+        
+        for(int i=0; i<branchLevelData.size(); i++) {
+        	try {
+        	branch_level[i][0] = branchLevelData.get(i).getBl1().doubleValue();
+        	branch_level[i][1] = branchLevelData.get(i).getBl2().doubleValue();
+        	branch_level[i][2] = branchLevelData.get(i).getBl3().doubleValue();
+        	branch_level[i][3] = branchLevelData.get(i).getBl4().doubleValue();
+        	branch_level[i][4] = branchLevelData.get(i).getBl5().doubleValue();
+        	branch_level[i][5] = branchLevelData.get(i).getBl6().doubleValue();
+        	branch_level[i][6] = branchLevelData.get(i).getBl7().doubleValue();
+        	branch_level[i][7] = branchLevelData.get(i).getBl8().doubleValue();
+        	branch_level[i][8] = branchLevelData.get(i).getBl9().doubleValue();
+        	branch_level[i][9] = branchLevelData.get(i).getBl10().doubleValue();
+        	branch_level[i][10] = branchLevelData.get(i).getBl11().doubleValue();
+        	branch_level[i][11] = branchLevelData.get(i).getBl12().doubleValue();
+        	branch_level[i][12] = branchLevelData.get(i).getBl13().doubleValue();
+        	}catch(Exception e) {
+        		e.printStackTrace();
+        	}
+        }
         
 		// exMatchIn
 		String nodeListData = task.getNodeList();
