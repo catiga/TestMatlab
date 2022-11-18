aa11efd5615dcd374ec3406e2a7776f94044a5fe
jackie lee <catiga03@gmail.com>
2022-11-18 17:46:26 +0800

    1

--- src/main/java/com/smartgrid/controller/HomeController.java
+++ src/main/java/com/smartgrid/controller/HomeController.java
@@ -5,7 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mathworks.toolbox.javabuilder.MWCellArray;
 import com.smartgrid.response.ProtObj;
+
+import CalculteTopo.CalculateTopo;
 
 @CrossOrigin
 @RestController
@@ -13,8 +16,11 @@ public class HomeController {
 	
 	@ResponseBody
 	@RequestMapping("/index")
-    public ProtObj index() {
-
+    public ProtObj index() throws Exception {
+		MWCellArray tableNodeArray = new MWCellArray(new int[] {3, 3});
+		CalculateTopo topoComputer = new CalculateTopo();
+		System.out.println(tableNodeArray);
+		System.out.println(topoComputer);
         return ProtObj.success("Hello Matlab! - smartgrid");
     }
 }
